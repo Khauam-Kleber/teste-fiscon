26155935e452da8de887330f716088cd725c75db
Khauam-Kleber <khauam@hotmail.com>
2022-09-18 19:44:20 -0300

    Projeto Teste Fiscon Iniciado 17:40 fim 19:40

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {MatSort} from '@angular/material/sort';
+import {MatTable, MatTableDataSource,} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'teste-fiscon';
+export class AppComponent implements AfterViewInit {
+ 
+  form: any;
+  listaCadastrados: any = []
+  displayedColumns: string[] = ['id', 'nome', 'telefone'];
+  @ViewChild(MatTable) table: MatTable<any>;
+  @ViewChild(MatSort) sort: MatSort;
+  
+  dataSource = new MatTableDataSource<any>();
+
+  constructor(private formBuilder:FormBuilder) { 
+    this.criarForm();
+    this.dataSource = new MatTableDataSource();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  criarForm(){
+    this.form = this.formBuilder.group({
+      id: [],
+      nome:  ['', Validators.required ],
+      telefone: ['', Validators.required ],
+    });
+  }
+
+  get formControls() { return this.form.controls; }
+
+  criarEPreencherTabela(){
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.form.get('id').setValue(this.listaCadastrados.length + 1)
+    this.listaCadastrados.push(this.form.value)
+    this.form.reset()
+    this.dataSource = new MatTableDataSource(this.listaCadastrados );
+    this.table.renderRows();
+
+    setTimeout(() => {
+      this.dataSource.sort = this.sort;
+    }, 1000);
+  }
+
+  limparForm(){
+    this.form.reset()
+  }
+
+  removeData() {
+    this.listaCadastrados.pop();
+    this.dataSource = new MatTableDataSource(this.listaCadastrados );
+    this.table.renderRows();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table'  
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatSortModule} from '@angular/material/sort';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatTableModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
